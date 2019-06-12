bada58b440e58b92f7091c4979c34bb7b7be6f6b
chen0808 <654446541@qq.com>
2019-06-12 09:29:15 +0800

    修改巡视内容界面bug

--- app/src/main/java/com/patrol/terminal/adapter/PatrolContentAdapter.java
+++ app/src/main/java/com/patrol/terminal/adapter/PatrolContentAdapter.java
@@ -8,6 +8,9 @@ import android.widget.AdapterView;
 import android.widget.EditText;
 import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.chad.library.adapter.base.BaseMultiItemQuickAdapter;
@@ -22,7 +25,6 @@ import com.patrol.terminal.bean.PatrolLevel1;
 import com.patrol.terminal.bean.PatrolLevel2;
 import com.patrol.terminal.utils.Constant;
 import com.patrol.terminal.utils.PictureSelectorConfig;
-import com.patrol.terminal.utils.RxRefreshEvent;
 import com.patrol.terminal.utils.SPUtil;
 import com.patrol.terminal.widget.ProgressDialog;
 
@@ -185,12 +187,23 @@ public class PatrolContentAdapter extends BaseMultiItemQuickAdapter<MultiItemEnt
                         }).show();
                     }
                 });
+                RelativeLayout rlContent = helper.getView(R.id.rl_content);
+                LinearLayout llContent = helper.getView(R.id.ll_content);
+                TextView tvItemContent = helper.getView(R.id.tv_item_content);
                 EditText etContent = helper.getView(R.id.et_content);
                 ImageView ivCommit = helper.getView(R.id.iv_commit);
                 ivCommit.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        uploadItem(item2, etContent.getText().toString());
+                        uploadItem(item2, etContent.getText().toString(), rlContent, llContent, tvItemContent);
+                    }
+                });
+                ImageView ivEdit = helper.getView(R.id.iv_edit);
+                ivEdit.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        rlContent.setVisibility(View.GONE);
+                        llContent.setVisibility(View.VISIBLE);
                     }
                 });
                 break;
@@ -237,7 +250,7 @@ public class PatrolContentAdapter extends BaseMultiItemQuickAdapter<MultiItemEnt
         PictureSelectorConfig.initMultiConfig2(activity, maxTotal);
     }
 
-    private void uploadItem(PatrolLevel2 item2, String content) {
+    private void uploadItem(PatrolLevel2 item2, String content, RelativeLayout rlContent, LinearLayout llContent, TextView tvContent) {
         params.put("task_id", toRequestBody(item2.getTask_id()));
         params.put("grade_id", toRequestBody("37E5647975394B1E952DC5D2796C7D73"));
         params.put("content", toRequestBody(content));
@@ -283,7 +296,10 @@ public class PatrolContentAdapter extends BaseMultiItemQuickAdapter<MultiItemEnt
                     protected void onSuccees(BaseResult t) throws Exception {
                         ProgressDialog.cancle();
                         Toast.makeText(mContext, "上传成功！", Toast.LENGTH_SHORT).show();
-                        RxRefreshEvent.publish("updateDefect@2");
+//                        RxRefreshEvent.publish("updateDefect@2");
+                        rlContent.setVisibility(View.VISIBLE);
+                        llContent.setVisibility(View.GONE);
+                        tvContent.setText(content);
                     }
 
                     @Override
