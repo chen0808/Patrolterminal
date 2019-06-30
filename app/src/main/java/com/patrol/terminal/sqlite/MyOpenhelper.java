70d690aa625142a62a00a1951d0313aae5616ec0
sanqian <457307783@qq.com>
2019-06-30 19:41:14 +0800

    linmeng

--- app/src/main/java/com/patrol/terminal/sqlite/MyOpenhelper.java
+++ app/src/main/java/com/patrol/terminal/sqlite/MyOpenhelper.java
@@ -33,6 +33,7 @@ public class MyOpenhelper extends SQLiteOpenHelper {
 //                    ")";
 
 
+    /*隐患文字识别数据库*/
     private static final String CREATE_DEFACT_TV_TABLE =
             "CREATE TABLE " + TABLE.AUTO_DEFACT_TABLE + "(" +
                     DefactTvColumns._ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
@@ -64,19 +65,6 @@ public class MyOpenhelper extends SQLiteOpenHelper {
 
         initDataBase(db);
     }
-
-//    private void initDataBase(SQLiteDatabase db) {
-//        String[] autoTvContents = mContext.getResources().getStringArray(R.array.auto_textview_contents);
-//
-//        ContentValues values;
-//        for (int i = 0; i < autoTvContents.length; i++) {
-//            values = new ContentValues();
-//            values.put(AutoTvColumns.QUEXIAN_ID, i);
-//            values.put(AutoTvColumns.CONTENT, autoTvContents[i]);
-//            db.insert(TABLE.AUTO_TEXTVIEW_TABLE, null, values);
-//            Log.d("linmeng", "values" + i + ":" + values);
-//        }
-//    }
 
     private void initDataBase(SQLiteDatabase db) {
         //从Excel取数据
@@ -114,10 +102,6 @@ public class MyOpenhelper extends SQLiteOpenHelper {
 
         @Override
         protected void onPreExecute() {
-//            progressDialog.setMessage("加载中,请稍后......");
-//            progressDialog.setCanceledOnTouchOutside(false);
-//            progressDialog.show();
-            Log.w("linmeng", "ExcelDataLoader onPreExecute:");
         }
 
         @Override
@@ -127,10 +111,6 @@ public class MyOpenhelper extends SQLiteOpenHelper {
 
         @Override
         protected void onPostExecute(ArrayList<DefactTvModel> defactTvModels) {
-//            if (progressDialog.isShowing()) {
-//                progressDialog.dismiss();
-//            }
-
             if(defactTvModels != null && defactTvModels.size() > 0){
                 //存在数据
                 ContentValues values;
@@ -139,15 +119,11 @@ public class MyOpenhelper extends SQLiteOpenHelper {
                     values.put(DefactTvColumns.LEVEL, defactTvModels.get(i).getLevel());
                     values.put(DefactTvColumns.CONTENT, defactTvModels.get(i).getContent());
                     mDb.insert(TABLE.AUTO_DEFACT_TABLE, null, values);
-                    Log.d("linmeng", "values" + i + ":" + values);
                 }
 
             }else {
                 //加载失败
-
-
             }
-
         }
     }
 
@@ -170,14 +146,8 @@ public class MyOpenhelper extends SQLiteOpenHelper {
             int sheetRows = sheet.getRows();
             int sheetColumns = sheet.getColumns();
 
-            Log.d(TAG, "the num of sheets is " + sheetNum);
-            Log.d(TAG, "the name of sheet is  " + sheet.getName());
-            Log.d(TAG, "total rows is 行=" + sheetRows);
-            Log.d(TAG, "total cols is 列=" + sheetColumns);
-
             for (int i = 0; i < sheetRows; i++) {
                 DefactTvModel defactTvModel = new DefactTvModel();
-                Log.d(TAG, "sheet.getCell(5,0).getContents()=" + sheet.getCell(5,0).getContents());
                 defactTvModel.setContent(sheet.getCell(5, i).getContents());
                 defactTvModel.setLevel(sheet.getCell(6, i).getContents());
                 countryList.add(defactTvModel);
