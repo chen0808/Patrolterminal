242b7de447aba37a4f13adcbb9425bd827029560
chen0808 <654446541@qq.com>
2019-07-01 14:48:10 +0800

    数据库初始化

---
+++ app/src/main/java/com/patrol/terminal/MyApp.java
@@ -0,0 +1,16 @@
+package com.patrol.terminal;
+
+import android.app.Application;
+
+import com.raizlabs.android.dbflow.config.FlowManager;
+
+public class MyApp extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        //初始化DBFLOW
+        FlowManager.init(this);
+
+    }
+}
+
