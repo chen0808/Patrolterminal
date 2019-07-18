4a405701558d3f10ff630a92a412dce67a563656
Administrator <litz52001@163.com>
2019-07-18 11:57:25 +0800

    修改显示问题

--- app/src/main/java/com/patrol/terminal/utils/Utils.java
+++ app/src/main/java/com/patrol/terminal/utils/Utils.java
@@ -1,11 +1,14 @@
 package com.patrol.terminal.utils;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.AssetManager;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.Build;
 import android.util.Log;
+import android.view.View;
 
 import com.patrol.terminal.R;
 import com.patrol.terminal.activity.CaptureActivity;
@@ -298,5 +301,20 @@ public class Utils {
             }
         }
         return false;
+    }
+
+    public static void hideClickStatus(Activity activity)
+    {
+        //隐藏虚拟按键，并且全屏
+        if (Build.VERSION.SDK_INT > 11 && Build.VERSION.SDK_INT < 19) { // lower api
+            View v = activity.getWindow().getDecorView();
+            v.setSystemUiVisibility(View.GONE);
+        } else if (Build.VERSION.SDK_INT >= 19) {
+            //for new api versions.
+            View decorView = activity.getWindow().getDecorView();
+            int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY ;
+            //  | View.SYSTEM_UI_FLAG_FULLSCREEN
+            decorView.setSystemUiVisibility(uiOptions);
+        }
     }
 }
