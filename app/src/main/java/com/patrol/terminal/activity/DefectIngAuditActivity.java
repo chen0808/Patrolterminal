7461126f41204fc632ba30ce6d49b4751b498a60
yinyanhua_007 <yinyanhua_007@163.com>
2019-07-24 17:40:54 +0800

    修改缺陷审核界面

--- app/src/main/java/com/patrol/terminal/activity/DefectIngAuditActivity.java
+++ app/src/main/java/com/patrol/terminal/activity/DefectIngAuditActivity.java
@@ -9,6 +9,9 @@ import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.bigkoo.pickerview.builder.TimePickerBuilder;
+import com.bigkoo.pickerview.listener.OnTimeSelectListener;
+import com.bigkoo.pickerview.view.TimePickerView;
 import com.patrol.terminal.R;
 import com.patrol.terminal.adapter.DefectPicAdapter;
 import com.patrol.terminal.base.BaseActivity;
@@ -19,9 +22,14 @@ import com.patrol.terminal.base.BaseUrl;
 import com.patrol.terminal.bean.DefectFragmentDetailBean;
 import com.patrol.terminal.bean.PatrolRecordPicBean;
 import com.patrol.terminal.utils.Constant;
+import com.patrol.terminal.utils.DateUatil;
+import com.patrol.terminal.utils.SPUtil;
 import com.patrol.terminal.widget.ProgressDialog;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 import butterknife.BindView;
@@ -69,14 +77,27 @@ public class DefectIngAuditActivity extends BaseActivity {
     TextView defectFindDepName;
     @BindView(R.id.defect_remark)
     TextView defectRemark;
+    @BindView(R.id.defect_deadline)
+    TextView defectDeadline;
     @BindView(R.id.deffect_img)
     TextView deffectImg;
     @BindView(R.id.defect_gridView)
     GridView defectGridView;
+    @BindView(R.id.reject)
+    TextView reject;
+    @BindView(R.id.stock_in)
+    TextView stockIn;
+    @BindView(R.id.review)
+    TextView review;
+    @BindView(R.id.turn_to_repair)
+    TextView turnToRepair;
+
+
     private DefectPicAdapter mGridViewAddImgAdapter; //展示上传的图片的适配器
     private ArrayList<String> mPicList = new ArrayList<>(); //上传的图片凭证的数据源
-    private int page = 1;
-
+    private String year;
+    private String month;
+    private String day;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -88,6 +109,18 @@ public class DefectIngAuditActivity extends BaseActivity {
     }
 
     private void initview(String id) {
+        String mJobType = SPUtil.getString(this, Constant.USER, Constant.JOBTYPE, Constant.RUNNING_SQUAD_LEADER);
+        if (mJobType.contains("_zz")) {
+            review.setVisibility(View.VISIBLE);
+            turnToRepair.setVisibility(View.VISIBLE);
+        } else {
+            review.setVisibility(View.GONE);
+            turnToRepair.setVisibility(View.GONE);
+        }
+
+        String time = DateUatil.getDay(new Date(System.currentTimeMillis()));
+        defectDeadline.setText(time);
+
         titleName.setText("缺陷审核");
         BaseRequest.getInstance().getService().getDefectDetail(id)
                 .subscribeOn(Schedulers.io())
@@ -146,10 +179,31 @@ public class DefectIngAuditActivity extends BaseActivity {
 
                         if ("一般".equals(bean.getGrade_name())) {
                             defectGradeName.setTextColor(getResources().getColor(R.color.blue));
+
+                            Calendar c = Calendar.getInstance();//获得一个日历的实例
+                            SimpleDateFormat sdf = new SimpleDateFormat("yyyy年MM月dd日");
+                            Date date = new Date(System.currentTimeMillis());
+                            c.setTime(date);//设置日历时间
+                            c.add(Calendar.MONTH,1);//在日历的月份上增加6个月
+                            defectDeadline.setText(sdf.format(c.getTime()));
                         } else if ("严重".equals(bean.getGrade_name())) {
                             defectGradeName.setTextColor(getResources().getColor(R.color.line_point_1));
+
+                            Calendar c = Calendar.getInstance();
+                            SimpleDateFormat sdf = new SimpleDateFormat("yyyy年MM月dd日");
+                            Date date = new Date(System.currentTimeMillis());
+                            c.setTime(date);
+                            c.add(Calendar.DATE,7);
+                            defectDeadline.setText(sdf.format(c.getTime()));
                         } else if ("危急".equals(bean.getGrade_name())) {
                             defectGradeName.setTextColor(getResources().getColor(R.color.line_point_0));
+
+                            Calendar c = Calendar.getInstance();
+                            SimpleDateFormat sdf = new SimpleDateFormat("yyyy年MM月dd日");
+                            Date date = new Date(System.currentTimeMillis());
+                            c.setTime(date);
+                            c.add(Calendar.DATE,1);
+                            defectDeadline.setText(sdf.format(c.getTime()));
                         }
                         defectGradeName.setText(bean.getGrade_name());
                         if (bean.getDeal_notes() != null) {
@@ -185,25 +239,26 @@ public class DefectIngAuditActivity extends BaseActivity {
                 });
     }
 
-    @OnClick({R.id.title_back})
+    @OnClick({R.id.title_back, R.id.defect_deadline})
     public void onViewClicked(View view) {
         switch (view.getId()) {
             case R.id.title_back:
                 finish();
                 break;
+            case R.id.defect_deadline:
+                showDay();
+                break;
         }
     }
 
     public void getPartrolRecord(String id) {
-
         ProgressDialog.show(this, false, "正在加载。。。。");
         BaseRequest.getInstance().getService()
                 .getDefectPic(id)
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(new BaseObserver<List<PatrolRecordPicBean>>(this) {
-
-
+                    
                     @Override
                     protected void onSuccees(BaseResult<List<PatrolRecordPicBean>> t) throws Exception {
                         if (t.getCode() == 1) {
@@ -241,5 +296,53 @@ public class DefectIngAuditActivity extends BaseActivity {
         intent.putStringArrayListExtra(Constant.IMG_LIST, mPicList);
         intent.putExtra(Constant.POSITION, position);
         startActivityForResult(intent, Constant.REQUEST_CODE_MAIN);
+    }
+
+    public void showDay() {
+        String time = DateUatil.getDay(new Date(System.currentTimeMillis()));
+        String[] years = time.split("年");
+        String[] months = years[1].split("月");
+        String[] days = months[1].split("日");
+        int curMonth = Integer.parseInt(months[0]);
+        int curYear = Integer.parseInt(years[0]);
+        int curDay = Integer.parseInt(days[0]);
+        if(curMonth == 1){
+            curMonth = 12;
+            curYear = curYear - 1;
+        } else {
+            curMonth = curMonth - 1;
+        }
+
+        Calendar selectedDate = Calendar.getInstance();//系统当前时间
+        Calendar startDate = Calendar.getInstance();
+        startDate.set(curYear, curMonth, curDay);
+        Calendar endDate = Calendar.getInstance();
+        endDate.set(2028, 2, 28);
+        //时间选择器 ，自定义布局
+        //选中事件回调
+        //是否只显示中间选中项的label文字，false则每项item全部都带有label。
+        TimePickerView pvTime = new TimePickerBuilder(this, new OnTimeSelectListener() {
+            @Override
+            public void onTimeSelect(Date date, View v) {//选中事件回调
+                String time = DateUatil.getDay(date);
+                defectDeadline.setText(time);
+                String[] times = time.split("年");
+                String[] months = times[1].split("月");
+                year = times[0];
+                month = months[0];
+                day = months[1].split("日")[0];
+            }
+        })
+                .setDate(selectedDate)
+                .setRangDate(startDate, endDate)
+                .setContentTextSize(18)
+                .setLineSpacingMultiplier(1.2f)
+                .setTextXOffset(0, 0, 0, 40, 0, -40)
+                .isCenterLabel(false) //是否只显示中间选中项的label文字，false则每项item全部都带有label。
+                .setDividerColor(0xFF24AD9D)
+                .setType(new boolean[]{true, true, true, false, false, false})
+                .setLabel("年", "月", "日", "时", "分", "秒")
+                .build();
+        pvTime.show();
     }
 }
