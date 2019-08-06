0ce62213c2636a551c26274a1589fcebdf76be85
Linmeng <414543604@qq.com>
2019-08-06 10:27:03 +0800

    检修总览

--- app/src/main/java/com/patrol/terminal/overhaul/OverhanlWorkFragment.java
+++ app/src/main/java/com/patrol/terminal/overhaul/OverhanlWorkFragment.java
@@ -27,72 +27,20 @@ import java.util.List;
 import butterknife.BindView;
 
 public class OverhanlWorkFragment extends BaseFragment {
-    @BindView(R.id.gridview)
-    GridView gridview;
+
     private GridViewAdapter5 weekAdapter;
     private List<MapUserInfo> results = new ArrayList<>();
 
     @Override
     protected View initView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_overhaul_other, container, false);
+        View view = inflater.inflate(R.layout.fragment_overhaul_work, container, false);
         return view;
     }
 
     @Override
     protected void initData() {
-        results.clear();
-        MapUserInfo mapUserInfo1 = new MapUserInfo();
-        mapUserInfo1.setUserImgId(R.mipmap.todo1);
-        mapUserInfo1.setUserName("工作票一");
-        results.add(mapUserInfo1);
 
-        MapUserInfo mapUserInfo2 = new MapUserInfo();
-        mapUserInfo2.setUserImgId(R.mipmap.todo2);
-        mapUserInfo2.setUserName("工作票二");
-        results.add(mapUserInfo2);
-
-        MapUserInfo mapUserInfo5 = new MapUserInfo();
-        mapUserInfo5.setUserImgId(R.mipmap.todo4);
-        mapUserInfo5.setUserName("控制卡");
-        results.add(mapUserInfo5);
-
-        weekAdapter = new GridViewAdapter5(getContext(), results);
-        gridview.setAdapter(weekAdapter);
-        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Intent intent = new Intent();
-                switch (i) {
-                    case 0:
-                        intent.setClass(getContext(), FirstWTicketActivity.class);
-                        break;
-                    case 1:
-                        intent.setClass(getContext(), SecondWTicketActivity.class);
-                        break;
-                    case 2:
-                       int entenType = Constant.IS_FZR_WRITE;
-                        AllControlCarBean workControlCardBean=new AllControlCarBean();
-                        intent.putExtra(Constant.CONTROL_CARD_ENTER_TYPE, entenType);
-                       ControlCardBean controlBean=new ControlCardBean();
-                        controlBean.setId("D831CD00AC0D4A68B6114F6A0A4A5282");
-                        intent.putExtra("id",controlBean);
-                        intent.putExtra("allControlBean",workControlCardBean);
-                        intent.setClass(getContext(), ControlCardActivity.class);
-                        break;
-                }
-                startActivity(intent);
-
-            }
-        });
     }
 
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-    }
 
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-    }
 }
