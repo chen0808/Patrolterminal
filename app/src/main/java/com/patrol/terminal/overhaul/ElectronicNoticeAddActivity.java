752deaef60ab5ee5c9393a2447cf6cf75a2ab44d
yinyanhua_007 <yinyanhua_007@163.com>
2019-08-13 16:27:02 +0800

    优化施工日志和电子公告

--- app/src/main/java/com/patrol/terminal/overhaul/ElectronicNoticeAddActivity.java
+++ app/src/main/java/com/patrol/terminal/overhaul/ElectronicNoticeAddActivity.java
@@ -1,8 +1,13 @@
 package com.patrol.terminal.overhaul;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -13,12 +18,17 @@ import com.bigkoo.pickerview.builder.TimePickerBuilder;
 import com.bigkoo.pickerview.listener.OnTimeSelectListener;
 import com.bigkoo.pickerview.view.TimePickerView;
 import com.patrol.terminal.R;
+import com.patrol.terminal.adapter.TssxPhotoAdapter;
 import com.patrol.terminal.base.BaseActivity;
 import com.patrol.terminal.utils.Constant;
 import com.patrol.terminal.utils.DateUatil;
+import com.patrol.terminal.utils.FileUtil;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -41,6 +51,12 @@ public class ElectronicNoticeAddActivity extends BaseActivity {
     TextView tvCompileDate;
     @BindView(R.id.tv_occur_date)
     TextView tvOccurDate;
+    @BindView(R.id.defect_gridView)
+    GridView defectGridView;
+
+    private List<String> photoList = new ArrayList<>();
+    private TssxPhotoAdapter photoAdapter;
+    private int position;//点击图片项
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -56,6 +72,16 @@ public class ElectronicNoticeAddActivity extends BaseActivity {
 
         titleSetting.setVisibility(View.VISIBLE);
         titleSettingTv.setText("发布");
+
+        photoAdapter = new TssxPhotoAdapter(this, photoList);
+        defectGridView.setAdapter(photoAdapter);
+        defectGridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                position = i;
+                startCamera();
+            }
+        });
     }
 
     @OnClick({R.id.title_back, R.id.tv_compile_date, R.id.tv_occur_date})
@@ -121,11 +147,36 @@ public class ElectronicNoticeAddActivity extends BaseActivity {
         pvTime.show();
     }
 
+    //打开相机
+    private void startCamera() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, Constant.DEFECT_REQUEST_CODE);
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (resultCode == RESULT_OK) {
-            if (requestCode == Constant.REQUEST_CODE_ADDRESS_BOOK) {
+            switch (requestCode) {
+                case Constant.DEFECT_REQUEST_CODE:
+                    Bundle bundle = data.getExtras();
+                    Bitmap bitmap = (Bitmap) bundle.get("data");
+                    String path = Environment.getExternalStorageDirectory().getPath()
+                            + "/MyPhoto/" + DateUatil.getDateStr() + "_" + String.valueOf(System.currentTimeMillis()) + ".jpg";
+                    try {
+                        //保存本地成功 刷新刷新数据添加到页面
+                        FileUtil.saveFile(bitmap, path);
+                        if (position == photoList.size()) {
+                            photoList.add(path);
+                        } else {
+                            photoList.set(position, path);
+                        }
+
+                        photoAdapter.notifyDataSetChanged();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    break;
             }
         }
     }
